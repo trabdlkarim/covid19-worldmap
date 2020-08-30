88f8822d9eeef497dd1d298eaea1d96fd2a210ec
trabdlkarim <trabdlkarim@programmer.net>
2020-08-30 18:16:56 +0300

    modified file

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,51 @@
 #!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os
+from flask import Flask
+from flask import (render_template, redirect, url_for)
+
+from covid19 import dataset as ds
+from covid19 import fmap
+from . import page, dataframe
+
+
+def create_app(test_config=None):
+
+    webapp = Flask(__name__, instance_relative_config=True)
+    webapp.config.from_mapping(SECRET_KEY='vkSW7bv5fe',DATABASE=os.path.join(webapp.instance_path, 'app_db.sqlite'),)
+
+    if test_config is None:
+        webapp.config.from_pyfile('config.py', silent=True)
+    else:
+        webapp.config.from_mapping(test_config)
+
+
+    try:
+        os.makedirs(webapp.instance_path)
+    except OSError:
+        pass
+
+    cdf = ds.get_top_infected_countries(n=20)
+    pairs = [(country,confirmed) for country,confirmed in zip(cdf.index,cdf['Confirmed'])]
+
+    covid19_df = ds.get_covid_df()
+
+
+    fm = fmap.get_folium_map()
+
+    html_map = fmap.apply_df_map(covid19_df,fm)
+
+    @webapp.route('/covidmap/')
+    def home():
+        return render_template('home.html',c_table =cdf,wmap=html_map,pairs=pairs)
+
+    @webapp.route('/')
+    def index():
+        return redirect(url_for('home'))
+
+    webapp.register_blueprint(page.bp)
+    webapp.register_blueprint(dataframe.bp)
+
+    return webapp
+
