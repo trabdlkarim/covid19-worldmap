7c3fb6513eade12def67db0e697cbadf572cbdf9
trabdlkarim <trabdlkarim@programmer.net>
2020-09-16 00:15:23 +0300

    rename file

---
+++ src/app/covid19/dataset.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 30 13:35:44 2020
+
+@author: trabdlkarim
+"""
+
+import pandas as pd
+import app
+
+
+
+def get_covid_df(did=1):
+    path = '/datasets/covid-19-dataset-%i.csv' % did
+    path = app.COVID_DATA_DIR + path 
+    return pd.read_csv(path)
+
+def get_top_infected_countries(did=1,n=10):
+    dataset = get_covid_df(did)
+    gby_df = dataset.groupby('Country_Region').sum()[['Confirmed', 'Deaths', 'Recovered','Active']]
+    cdf = gby_df.nlargest(n,'Confirmed')[['Confirmed']]
+    return cdf
